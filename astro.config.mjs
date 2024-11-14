89c300d3e98fde2adb5da9d0a61db58f657f7643
Salwan K <salwankajas@gmail.com>
2024-11-14 15:30:27 +0530

    made templates/data.json public

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,22 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  vite: {
+    plugins: [
+      viteStaticCopy({
+        targets: [
+          {
+            src: 'src/assets/templates/*',
+            dest: 'public/templates/'
+          }
+        ],
+        hook: 'build',
+      })
+    ]
+  }
 });
